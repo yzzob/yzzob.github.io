08084e95a6cfbe3ff7e89fe9ef09615f194f399e
yzzob <2672510873@qq.com>
2024-04-19 21:24:45 +0800

    Site updated: 2024-04-19 21:24:43

--- plugin/motto/motto.js
+++ plugin/motto/motto.js
@@ -2,7 +2,8 @@ var mingyan = [
   '海阔凭鱼跃,天高任鸟飞。<br1>阮阅《诗话总龟前集》',
   '慕强便会世俗<br1>鲁迅',
   '你想活出怎样的人生<br1>宫崎骏',
-  '滚滚长江东逝水，浪花淘尽英雄。是非成败转头空。<br>青山依旧在，几度夕阳红。<br>白发渔樵江渚上，惯看秋月春风。一壶浊酒喜相逢。<br>古今多少事，都付笑谈中。<br1>杨慎 《临江仙·滚滚长江东逝水》'
+  '滚滚长江东逝水，浪花淘尽英雄。是非成败转头空。<br>青山依旧在，几度夕阳红。<br>白发渔樵江渚上，惯看秋月春风。一壶浊酒喜相逢。<br>古今多少事，都付笑谈中。<br1>杨慎 《临江仙·滚滚长江东逝水》',
+  'I love you so much,<br>I just don\'t like you anymore.<br1>《one day》'
 ];
 function getMingYan(){
   return mingyan[Math.floor(Math.random() * mingyan.length)];
